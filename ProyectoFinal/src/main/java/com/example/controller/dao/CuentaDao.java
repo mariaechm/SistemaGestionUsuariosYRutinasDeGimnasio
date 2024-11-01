c7da832b47a1a85d606c5b0db0234c3bea8f664d
Maria Elizabeth Chuico Medina <166523237+mariaechm@users.noreply.github.com>
2024-11-01 14:55:02 -0500

    Creacion de Proyecto
    Models-Dao-Services

---
+++ ProyectoFinal/src/main/java/com/example/controller/dao/CuentaDao.java
@@ -0,0 +1,5 @@
+package com.example.controller.dao;
+
+public class CuentaDao {
+    
+}
---
+++ ProyectoFinal/src/main/java/com/example/controller/tda/list/LinkedList.java
@@ -0,0 +1,178 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.controller.tda.list;
+import com.example.controller.exception.ListEmptyException;
+
+/**
+ *
+ * @author Grupo6
+ */
+
+public class LinkedList<E> {
+    private Node<E> header;
+    private Node<E> last;
+    private Integer size;
+
+    public LinkedList() {
+        this.header = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    public Boolean isEmpty() {
+        return (this.header == null || this.size == 0);
+    }
+
+    private void addHeader(E dato) {
+        Node<E> help;
+        if (isEmpty()) {
+            help = new Node<>(dato);
+            header = help;
+
+        } else {
+            Node<E> healpHeader = this.header;
+            help = new Node<>(dato, healpHeader);
+            this.header = help;
+            //this.size++;
+        }
+        this.size++;
+    }
+
+    private void addLast(E info) {
+        Node<E> help;
+        if (isEmpty()) {
+            addHeader(info);
+        } else {
+            help = new Node<>(info, null);
+            last.setNext(help);
+            last = help;
+            this.size++;
+        }
+    }
+
+    public void add(E info) {
+        addLast(info);
+    }
+
+    private Node<E> getNode(Integer index) throws ListEmptyException, IndexOutOfBoundsException {
+        if (isEmpty()) {
+            throw new ListEmptyException("Error, List empty");
+        } else if (index.intValue() < 0 || index.intValue() >= this.size) {
+            throw new IndexOutOfBoundsException("Error, fuera de rango");
+        } else if (index.intValue() == 0) {
+            return header;
+        } else if (index.intValue() == (this.size - 1)) {
+            return last;
+        } else {
+            Node<E> search = header;
+            int cont = 0;
+            while (cont < index.intValue()) {
+                cont++;
+                search = search.getNext();
+            }
+            return search;
+        }
+    }
+
+    private E getFirst() throws ListEmptyException {
+        if (isEmpty()) {
+            throw new ListEmptyException("Error, Lista vacía");
+        }
+        return header.getInfo();
+    }
+
+    private E getLast() throws ListEmptyException {
+        if (isEmpty()) {
+            throw new ListEmptyException("Error, Lista vacía");
+        }
+        return last.getInfo();
+    }
+
+    public E get(Integer index) throws ListEmptyException, IndexOutOfBoundsException {
+        if (isEmpty()) {
+            throw new ListEmptyException("Error, Lista vacía");
+        } else if (index.intValue() < 0 || index.intValue() >= this.size.intValue()) {
+            throw new IndexOutOfBoundsException("Error, fuera de rango");
+        } else if (index.intValue() == 0) {
+            return header.getInfo();
+        } else if (index.intValue() == (this.size - 1)) {
+            return last.getInfo();
+        } else {
+            Node<E> search = header;
+            int cont = 0;
+            while (cont < index.intValue()) {
+                cont++;
+                search = search.getNext();
+            }
+            return search.getInfo();
+        }
+    }
+
+    public void add(E info, Integer index) throws ListEmptyException, IndexOutOfBoundsException {
+        if (isEmpty() || index.intValue() == 0) {
+            addHeader(info);
+        } else if (index.intValue() == this.size.intValue()) {
+            addLast(info);
+        } else {
+            Node<E> search_preview = getNode(index - 1);
+            Node<E> search = getNode(index);
+            Node<E> help = new Node<>(info, search);
+            search_preview.setNext(help);
+            this.size++;
+        }
+    }
+
+    /*** END BY POSITION */
+
+    public void reset() {
+        this.header = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("List Data \n");
+        try {
+            Node<E> help = header;
+            while (help != null) {
+                sb.append(help.getInfo()).append(" -> ");
+                help = help.getNext();
+            }
+        } catch (Exception e) {
+            sb.append(e.getMessage());
+        }
+
+        return sb.toString();
+    }
+
+    public Integer getSize() {
+        return this.size;
+    }
+
+    public E[] toArray(){
+        E[] matrix = null;
+        if (!isEmpty()) {
+            Class clazz = header.getInfo().getClass();
+            matrix = (E[]) java.lang.reflect.Array.newInstance(clazz, size);
+            Node<E> aux = header;
+            for(int i=0; i<size; i++){
+                matrix[i] = aux.getInfo();
+                aux = aux.getNext();
+            }
+        }
+        return matrix;
+    }
+
+    public LinkedList<E> toList(E[] matrix){
+        reset();
+        for(int i = 0; i < matrix.length; i++){
+            this.add(matrix[i]);
+        }
+        return this;
+    }
+
+}
+
